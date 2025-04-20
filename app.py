ec6f2f3232cb379dea34bc373582a355b31361ce
theookyere <142364470+theookyere@users.noreply.github.com>
2025-04-21 01:08:04 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ ABUSIVE_WORDS = [
     'cancer', 'kanker', 'nazi', 'racist', 'neuk', 'pis', 'dick', 'damn', 'retard', 'gay', 'homo', 'faggot', 'slut', 'whore'
 ]
 
+# Expanded abusive/negative phrases
+ABUSIVE_PHRASES = [
+    "get out", "leave me alone", "i'm angry", "i'm furious", "i'm livid", "you're infuriating", "don't talk to me", "go away", "i hate this", "i can't stand it", "that's enough", "stop it", "you're a jerk", "you're a fool", "you're a waste of space", "i'm sick of you", "i'm done with this", "don't push me", "you're testing me", "i'm warning you", "you should be ashamed of yourself", "whatever", "it doesn't matter", "who cares", "that's not my problem", "it's fine", "i don't care", "it's no big deal", "don't worry about it", "just forget about it", "it's over", "let it go", "it's pointless to argue", "you're impossible", "i can't deal with this", "it's hopeless", "it's going nowhere"
+]
+
 # Very basic name pattern (capitalized word, not at start of sentence)
 NAME_PATTERN = re.compile(r'(?:\b[A-Z][a-z]{2,}\b)')
 
@@ -29,11 +34,14 @@ def add_post():
     text = data.get('text', '').strip()
     if not text:
         return jsonify({'error': 'Text is required'}), 400
-    # Abusive word filter
+    # Abusive word/phrase filter
     lowered = text.lower()
     for word in ABUSIVE_WORDS:
         if word in lowered:
             return jsonify({'error': 'Abusive or inappropriate language is not allowed.'}), 400
+    for phrase in ABUSIVE_PHRASES:
+        if phrase in lowered:
+            return jsonify({'error': 'Negatieve of beledigende berichten zijn niet toegestaan.'}), 400
     # Name detection (Dutch/English style)
     # Disallow any capitalized word not at the start, and not "Ik", "We", "Oranje", etc.
     allowed = {'Ik', 'We', 'Ons', 'Onze', 'Oranje', 'Koningsdag', 'Koning', 'Koningin', 'Nederland', 'NL', 'Feest'}
